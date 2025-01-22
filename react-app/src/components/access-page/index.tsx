7c6ec10d476ffc8044967c366fcc61440d56bf9a
Alexandre <you@example.com>
2025-01-22 16:18:49 +0000

    chore: stages

--- react-app/src/components/access-page/index.tsx
+++ react-app/src/components/access-page/index.tsx
@@ -1,9 +1,7 @@
+import { Group } from "@chakra-ui/react";
 import { Fragment } from "react";
 import { Page } from "../ui/page";
 import { DialogUserForm, LoginForm } from "../user";
-import { Button, createToaster, Group } from "@chakra-ui/react";
-import { toaster } from "../ui/toaster-fn";
-import { Toaster } from "../ui/toaster";
 
 export const AccessPage = () => {
   return (
