ed2c7a69c728d3febeb0f04e4bd243a42f451634
Alexandre Sage (ASE) <alexandre.sage@adsoftware.fr>
2025-12-31 11:09:44 +0100

    chore: add old api key

--- backend/migrations/20251203155111_insert_storm_glass_keys.sql
+++ backend/migrations/20251203155111_insert_storm_glass_keys.sql
@@ -1,3 +1,8 @@
 INSERT INTO api_keys (id, key, daily_usage, date) VALUES
 ('fe93113c-cf63-11f0-a148-0242ac130003', 'fe93113c-cf63-11f0-a148-0242ac130003-fe9311fa-cf63-11f0-a148-0242ac130003', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+('dbacbe86-f267-496f-a132-d2f404723dcb', 'bf91a76a-8545-11ee-8fd1-0242ac130002-bf91a7d8-8545-11ee-8fd1-0242ac130002', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+('42319a70-a940-4186-9868-fb8039e16f67', '9a90a39c-86cd-11ee-8fd1-0242ac130002-9a90a414-86cd-11ee-8fd1-0242ac130002', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+('b6804c32-2d81-4193-aae2-014f7fa6fe5e', '82cf2668-86df-11ee-8fd1-0242ac130002-82cf26d6-86df-11ee-8fd1-0242ac130002', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+('a2883458-2506-4991-b90c-f39b35787356', 'b772d770-86df-11ee-935b-0242ac130002-b772d81a-86df-11ee-935b-0242ac130002', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
+('0d740a53-0dbf-4a4b-b3a0-e27d87967ece', 'aa2398a4-b1f3-11ee-950b-0242ac130002-aa239962-b1f3-11ee-950b-0242ac130002', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
 ('b3769be6-cf64-11f0-bd1c-0242ac130003', 'b3769be6-cf64-11f0-bd1c-0242ac130003-b3769c54-cf64-11f0-bd1c-0242ac130003', 0, strftime('%Y-%m-%dT%H:%M:%SZ', 'now'));
